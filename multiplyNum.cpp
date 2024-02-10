65299ddd29c356e37d18a3607e1a8c772d80260a
sanas <sanasmath@gmail.com>
2024-02-10 20:34:33 +0530

    multiply two numbers

---
+++ multiplyNum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double num1,num2;
+    cout<<"Enter the two numbers\n";
+    cin>>num1;
+    cin>>num2;
+    cout<<"product is : "<<num1*num2;
+
+
+    return 0;
+}
